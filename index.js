532bd3223200694369eaab905f9e4f8ce76abdb7
Frazhuz <91553591+Frazhuz@users.noreply.github.com>
2025-04-12 02:08:11 +0400

    СЛАВА ВБИЛЧУ тест

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ client.on('messageCreate', message => {
   if (message.author.bot) return;
 
   if (message.content === '!ping') {
-    message.channel.send('Pong!');
+    message.channel.send('СЛАВА ВБИЛЧУ!');
   }
 });
 
