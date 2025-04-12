7a5b27e25637dd3bb68adb456a56e9fda348aeec
Frazhuz <91553591+Frazhuz@users.noreply.github.com>
2025-04-12 19:39:30 +0400

    Add say command

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
-const axios = require('axios');
-const path = require('path');
+const commands = {
+  ping: require('./commands/ping'),
+  say: require('./commands/say')
+};
 
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
+    GatewayIntentBits.GuildMessages
   ]
 });
+
 
 client.login(DISCORD_TOKEN).catch(err => {
   console.error('Ошибка входа:', err);
@@ -22,11 +24,17 @@ client.on('ready', () => {
     console.log(`Бот успешно запущен как ${client.user.tag}`);
 });
 
-
 client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!');
+    switch (interaction.commandName) {
+    case 'ping':
+      await commands.ping.execute(interaction);
+      break;
+    case 'say':
+      await commands.say.execute(interaction);
+      break;
+    default:
+      console.warn(`Неизвестная команда: ${interaction.commandName}`);
     }
 });
