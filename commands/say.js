5a8bfacb5a5e96b130570962766b2c0215ee5322
Frazhuz <91553591+Frazhuz@users.noreply.github.com>
2025-04-13 03:15:08 +0400

    Update say.js for test

--- commands/say.js
+++ commands/say.js
@@ -1,6 +1,22 @@
 module.exports = {
     execute: async (interaction) => {
         const text = interaction.options.getString('text');
+
+        if (!interaction) {
+            console.error('Параметр "interaction" не получен');
+            return await interaction.reply({ 
+                content: '❌ Не получен interaction!', 
+                ephemeral: true 
+            });
+        }
+
+        if (!text) {
+            console.error('Параметр "text" не получен');
+            return await interaction.reply({ 
+                content: '❌ Не указан текст!', 
+                ephemeral: true 
+            });
+        }
         await interaction.reply(text);
     }
 };
