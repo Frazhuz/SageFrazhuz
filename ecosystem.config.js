d5c74110dcf65d55c88e84d71e38cef3da34c057
Frazhuz <91553591+Frazhuz@users.noreply.github.com>
2025-04-13 13:11:19 +0200

    git push git push --tags pm2 settings

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports={
+  apps:[{
+    name:"bot",
+    script:"./index.js",
+    watch:true,
+    ignore_watch:["node_modules","logs"],
+    log_date_format:"YYYY-MM-DD HH:mm:ss",
+    out_file:"./logs/bot-out.log",
+    error_file:"./logs/bot-error.log",
+    merge_logs:true,
+    time:true,
+    autorestart:true
+  }]
+};
