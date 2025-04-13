5a0054344f569cdba007b4dd6b4c3611d3c2c0dd
Frazhuz <91553591+Frazhuz@users.noreply.github.com>
2025-04-13 15:27:13 +0400

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,8 @@ module.exports={
     watch:true,
     ignore_watch:["node_modules","logs"],
     log_date_format:"YYYY-MM-DD HH:mm:ss",
-    out_file:"./logs/bot-out.log",
-    error_file:"./logs/bot-error.log",
+    out_file:"./logs/bot-combined.log",
+    error_file:"./logs/bot-combined.log",
     merge_logs:true,
     time:true,
     autorestart:true
