44879e7729daf56d1eabb2f153f0fffea073a10e
Frazhuz <91553591+Frazhuz@users.noreply.github.com>
2025-04-17 02:52:15 +0400

    Update errorHandler.js

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -7,7 +7,7 @@ class KeyError extends Error {
     this.key = key;
     this.identificator = key;
     this.cause = cause;
-    if(cause.stack) this.stack = cause.stack;
+    if (cause?.stack) this.stack = cause.stack;
     this.messageArgs = messageArgs;
   }
 }
