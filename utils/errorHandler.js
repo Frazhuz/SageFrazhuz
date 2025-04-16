0f732e5bd66e68a1e19e2b3e362b295331f58723
Frazhuz <91553591+Frazhuz@users.noreply.github.com>
2025-04-17 03:00:22 +0400

    Update errorHandler.js

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -51,7 +51,7 @@ class ErrorHandler {
   
   static #constructBasicMessage(messages, error) {
     const key = error.key;
-    const message = (error.message ?? messages.key.(...error.messageArgs)) + 
+    const message = (error.message ?? messages.key?.(...error.messageArgs)) + 
       (error.cause ? `\nCause: ${error.cause.message}` : '');
     return message;
   }
