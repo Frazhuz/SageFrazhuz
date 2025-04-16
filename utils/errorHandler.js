230bd5f8f7c03666af15bbe7c38bb71d405c4e3e
Frazhuz <91553591+Frazhuz@users.noreply.github.com>
2025-04-17 02:05:30 +0400

    Update errorHandler.js

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -92,7 +92,7 @@ class ErrorHandler {
       return;
     }
     const error = this.#constructError(messages, options);
-    console.error(`${error.name}: ${error.message}\n${error.stack}` + error.cause.message + error.cause.stack);
+    console.error(`${error.name}: ${error.message}\n${error.stack}`);
   }
 
   static #log = this.log.bind(this, this.ERROR_MESSAGES);
