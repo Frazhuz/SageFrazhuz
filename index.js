08400b20038a224ac7ec2e664f98e80b822a95fe
Frazhuz <91553591+Frazhuz@users.noreply.github.com>
2025-04-17 02:11:09 +0400

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ Promise.all([
   })
   .then(() => console.log('🔗 Connecting to Discord...'))
   .catch((cause) => {
-    ErrorHandler.log.(ERROR_MESSAGES, { key: 'FAILED_INITIALIZE', cause: cause });
+    ErrorHandler.log(ERROR_MESSAGES, { key: 'FAILED_INITIALIZE', cause: cause });
     process.exit(1);
   });
   
