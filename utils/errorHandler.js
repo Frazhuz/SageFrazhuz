019f58c5abb1ebbdaa3e53c3c5a411d8deb7a69d
Frazhuz <91553591+Frazhuz@users.noreply.github.com>
2025-04-17 12:52:03 +0400

    Update errorHandler.js

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,5 +1,5 @@
 class KeyError extends Error {
-  constructor({ name: 'KeyError', message, reply, key, identificator, cause, interaction, ...messageArgs } = {}) {
+  constructor({ name = 'KeyError', message, reply, key, identificator, cause, interaction, ...messageArgs } = {}) {
     super(message, {cause});
     this.interaction = interaction;
     this.name = name;
