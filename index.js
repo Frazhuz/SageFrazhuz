52b6459b6df3b16e9280f7991e20f32bbb66f6bc
Frazhuz <91553591+Frazhuz@users.noreply.github.com>
2025-04-18 17:59:23 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
-const loadCommand = require('./utils/commandLoader.js');
+const CommandLoader = require('./utils/commandLoader.js');
 const { KeyError, genReportError } = require('./utils/errorHandler.js');
 
 const ERROR_MESSAGES = {
   NO_DISCORD_TOKEN: () => 'Missing DISCORD_TOKEN in .env',
-  FAILED_LOGIN: () => 'Failed to login.',
+  FAILED_LOGIN: () => 'Failed to login or load commands.',
   FAILED_LOAD: () => 'Unexpected error loading commands. None of the commands are loaded.',
-  UNKNOWN_COMMAND: (name) => `Attempted to call unknown command: ${name}`,
+  UNKNOWN_COMMAND: () => `Attempted to call unknown command.`,
   UNHANDLED_REJECTION: () => 'Unhandled Promise Rejection.',
   UNCAUGHT_EXCEPTION: () => 'Uncaught Exception.',
 };
@@ -18,6 +18,8 @@ const ERROR_REPLIES = {
 
 const reportError = genReportError(ERROR_MESSAGES);
 
+if (!process.env.DISCORD_TOKEN) reportError({ key: 'NO_DISCORD_TOKEN' });
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -25,23 +27,20 @@ const client = new Client({
   ]
 });
 
-if (!process.env.DISCORD_TOKEN) reportError({ key: 'NO_DISCORD_TOKEN' });
-
-client.login(process.env.DISCORD_TOKEN).then(
-  () => reportError.client = client,
-  cause => reportError({ key: 'FAILED_LOGIN', cause: cause })
-);
-
 let commands = {};
-Promise.all([
-  loadCommand('ping', './commands/ping.js'),
-  loadCommand('say', '../commands/say.js'),
-  loadCommand('import', '../commands/import/import.js')
-])
-  .then(
-    commandArray => commands = Object.fromEntries(commandArray),
-    cause => reportError({ key: 'FAILED_LOGIN', cause: cause })
-  );
+client.login(process.env.DISCORD_TOKEN)
+  .then(() => {
+    reportError.client = client;
+    CommandLoader.reportError = genReportError(CommandLoader.ERROR_MESSAGES, client);
+    return Promise.all([
+      CommandLoader.load('ping', './commands/ping.js'),
+      CommandLoader.load('say', '../commands/say.js'),
+      CommandLoader.load('import', '../commands/import/import.js'),
+    ]);
+  })
+  .catch(cause => reportError({ key: 'FAILED_LOGIN', cause: cause }))
+  .then(commandArray => commands = Object.fromEntries(commandArray))
+  .catch(cause => reportError({ key: 'FAILED_LOAD', cause: cause }));
   
 process.on(
   'unhandledRejection',
@@ -56,15 +55,15 @@ process.on(
 client.on('ready', () => console.log(`🤖 Bot logged in as ${client.user.tag}`));
 
 client.on('interactionCreate', async (interaction) => {
-  const ReportError = genReportError(ERROR_MESSAGES, client, ERROR_REPLIES, interaction);
+  const ReportInteractionError = genReportError(ERROR_MESSAGES, client, ERROR_REPLIES, interaction);
   if (!interaction.isChatInputCommand()) return;
   const command = commands[interaction.commandName];
   if (!command) {
-    await ReportError({key: 'UNKNOWN_COMMAND', messageArgs: interaction.commandName});
+    await ReportInteractionError({key: 'UNKNOWN_COMMAND'});
     return;
   }
 
-  const ReportInternalError = genReportError(
+  const ReportCommandError = genReportError(
     command.ERROR_MESSAGES, 
     client,
     command.ERROR_REPLIES,
@@ -74,7 +73,7 @@ client.on('interactionCreate', async (interaction) => {
   try {
     command.execute(interaction);
   } catch(error) {
-    ReportInternalError(error)
+    ReportCommandError(error)
   };
 });
 
